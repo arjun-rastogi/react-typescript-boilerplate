1a924a4abe527d85a83bac08cf55c90980499c95
arjun-rastogi <arjun.rastogi2001@gmail.com>
2023-05-20 13:40:26 +0530

    Backend Initialised

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const connection = require("./connection");
+const authRoute = require("./routes/userRoutes");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+// Routing for our product file.
+app.use("/api", authRoute);
+
+module.exports = app;
