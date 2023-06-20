6c600e17916ccdc4dd3015cc951d2d80dea4bd99
arjun-rastogi <arjun.rastogi2001@gmail.com>
2023-06-20 12:07:53 +0530

    Utility Upgraded

---
+++ frontend/src/utils/handleMultipleUpload.tsx
@@ -0,0 +1,38 @@
+interface CustomFile extends File {
+  status: boolean;
+}
+
+const handleMultipleUpload = (
+  files: FileList | null,
+  MAX_COUNT: number
+): [CustomFile[], { status: boolean }] => {
+  if (!files) {
+    return [[], { status: false }];
+  }
+
+  const uploaded: CustomFile[] = Array.from(files) as CustomFile[];
+  let limitExceeded = true;
+
+  Array.from(files).forEach((file) => {
+    if (uploaded.findIndex((f) => f.name === file.name) === -1) {
+      uploaded.push({ ...file, status: true });
+      if (uploaded.length === MAX_COUNT) {
+        limitExceeded = true;
+        return;
+      }
+      if (uploaded.length > MAX_COUNT) {
+        limitExceeded = false;
+        return;
+      }
+    }
+  });
+
+  if (uploaded.length > MAX_COUNT) {
+    alert(`You can only add a maximum of ${MAX_COUNT} files`);
+    limitExceeded = false;
+  }
+
+  return [uploaded, { status: limitExceeded }];
+};
+
+export default handleMultipleUpload;
