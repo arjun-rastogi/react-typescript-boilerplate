48b38f577da0f928aa2ad8dedf4eeb848ae2ebe8
arjun-rastogi <arjun.rastogi2001@gmail.com>
2023-06-20 12:11:51 +0530

    UI Upgraded

---
+++ frontend/src/common/delete.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+type Props = {
+  onClick: (id: string) => void;
+};
+
+function Delete({ onClick }: Props) {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  const handleClose = () => {
+    setOpen(!open);
+  };
+
+  const handleDelete = () => {
+    onClick("");
+    setOpen(!open);
+  };
+
+  return (
+    <>
+      <Link to="#" className="btn-edt" onClick={handleOpen}>
+        <i className="fa fa-trash" aria-hidden="true"></i>
+      </Link>
+
+      <div
+        className={open ? "modal fade show" : "modal fade"}
+        style={{ display: open ? "block" : "none" }}
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="staticBackdropLabel">
+                Confirmation Delete
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                onClick={handleClose}
+              ></button>
+            </div>
+            <div className="modal-body">
+              Are you sure you want to delete this record...?!
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button
+                type="button"
+                onClick={handleDelete}
+                className="btn btn-primary"
+              >
+                Understood
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={open ? "modal-backdrop " : ""}></div>
+    </>
+  );
+}
+
+export default Delete;
